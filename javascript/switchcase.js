c2de10f16054785281f0c794e5b59effa5d4c7f7
Brown-Sage <luckytak60@gmail.com>
2024-10-09 22:05:42 +0530

    JS: learning

---
+++ javascript/switchcase.js
@@ -0,0 +1,12 @@
+let namee = "aaryan";
+console.log(namee.charAt(5));
+switch (namee.charAt(3)) {
+    case "a":
+        console.log("yes");
+        break;
+    case "r":
+        console.log("no");
+        break;
+    default:
+        console.log("who knows");
+}
