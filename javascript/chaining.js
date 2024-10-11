5a7b45787793c8c528ebd17c1893a27c0c2dd782
Brown-Sage <luckytak60@gmail.com>
2024-10-11 23:20:21 +0530

    Learning method chaining and some other stuff

---
+++ javascript/chaining.js
@@ -0,0 +1,22 @@
+// const info = {
+//     hmm1: function () {
+//         console.log("hmm1");
+//         return this;
+//     },
+//     hmm2: function () {
+//         console.log("hmm2");
+//         return this;
+//     }
+// };
+// info.hmm1().hmm2();
+// function func1() {
+//     return 2;
+// }
+
+// function func2() {
+//     return 4;
+// }
+
+// let a = (func1(), func2())
+
+// console.log(a);
