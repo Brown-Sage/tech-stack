7493b0500fe656ae01057839ec8f3d14dbbdc685
Brown-Sage <luckytak60@gmail.com>
2025-01-03 16:17:25 +0530

    Trying HashSet with strings

--- DSAJAVA/Hashing/hashset/hashsett.java
+++ DSAJAVA/Hashing/hashset/hashsett.java
@@ -1,10 +1,10 @@
 import java.util.HashSet;
 import java.util.Iterator;
-
+//                                  HASHSET DOES NOT HAVE ANY PERTICULAR ORDER OF STORING ELEMENTS
 class hashsett { // Class name should ideally be PascalCase (e.g., HashSetExample)
-    static HashSet<Integer> hs = new HashSet<>(); // Static HashSet to store integers
+    static HashSet<String> hs = new HashSet<>(); // Static HashSet to store integers
 
-    static int search(int value) { // Static method to search for a value
+    static int search(String value) { // Static method to search for a value
         if (hs.contains(value)) { // Check if the HashSet contains the given value
             System.out.println(value + " is present in the hashset"); // Print if the value is found
             return 1; // Return 1 to indicate the value is present
@@ -15,17 +15,17 @@ class hashsett { // Class name should ideally be PascalCase (e.g., HashSetExampl
     }
 
     public static void main(String[] args) { // Main method where the program starts
-        hs.add(2); // Add elements to the HashSet
-        hs.add(3);
-        hs.add(4);
-        hs.add(5);
-        hs.add(6);
+        hs.add("ad"); // Add elements to the HashSet
+        hs.add("b");
+        hs.add("c");
+        hs.add("d");
+        hs.add("e");
 
-        hs.remove(3); // Remove the element 3 from the HashSet
+        hs.remove("e"); // Remove the element 3 from the HashSet
 
         System.out.println("size of the set is " + hs.size()); // Print the current size of the HashSet
 
-        search(3); // Search for the element 3 (which was removed)
+        search("f"); // Search for the element 3 (which was removed)
 
         System.out.println(hs); // Print the contents of the HashSet (order is not guaranteed)
 
